859058e883f8263c01ad876e9911dff5365babf3
Jose Alberto Yepez <josealbertoyepez93@gmail.com>
2025-06-02 22:58:21 +0200

    feat: expand conversation workflow with new question and decision nodes

--- philoagents-api/src/philoagents/application/conversation_service/workflow/cluedo_graph.py
+++ philoagents-api/src/philoagents/application/conversation_service/workflow/cluedo_graph.py
@@ -10,6 +10,9 @@ from philoagents.application.conversation_service.workflow.cluedo_nodes import (
     conversation_node,
     crime_case_node,
     select_suspect_node,
+    get_question_node,
+    get_decision_continue_conversation_node,
+    get_decision_select_or_exit_node
     # summarize_conversation_node,
     # retriever_node,
     # summarize_context_node,
@@ -25,7 +28,10 @@ def create_workflow_graph():
     # Add all nodes
     graph_builder.add_node("crime_case_node", crime_case_node)
     graph_builder.add_node("select_suspect_node", select_suspect_node)
+    graph_builder.add_node("get_question_node", get_question_node)
     graph_builder.add_node("conversation_node", conversation_node)
+    graph_builder.add_node("get_decision_continue_conversation_node", get_decision_continue_conversation_node)
+    graph_builder.add_node("get_decision_select_or_exit_node", get_decision_select_or_exit_node)
     # graph_builder.add_node("retrieve_philosopher_context", retriever_node)
     # graph_builder.add_node("summarize_conversation_node", summarize_conversation_node)
     # graph_builder.add_node("summarize_context_node", summarize_context_node)
@@ -34,7 +40,26 @@ def create_workflow_graph():
     # Define the flow
     graph_builder.add_edge(START, "crime_case_node")
     graph_builder.add_edge("crime_case_node", "select_suspect_node")
-    graph_builder.add_edge("select_suspect_node", "conversation_node")
+    graph_builder.add_edge("select_suspect_node", "get_question_node")
+    graph_builder.add_edge("get_question_node", "conversation_node")
+    graph_builder.add_edge("conversation_node", "get_decision_continue_conversation_node")
+    graph_builder.add_conditional_edges(
+        "get_decision_continue_conversation_node",
+        get_decision_continue_conversation_node,
+        {
+            "": "get_question_node",
+            "yes": "get_question_node",
+            "no": "get_decision_select_or_exit_node"
+        }
+    )
+    graph_builder.add_conditional_edges(
+        "get_decision_select_or_exit_node",
+        get_decision_select_or_exit_node,
+        {
+            "select": "select_suspect_node",
+            "exit": END
+        }
+    )
     # graph_builder.add_conditional_edges(
     #     "conversation_node",
     #     tools_condition,
@@ -47,7 +72,6 @@ def create_workflow_graph():
     # graph_builder.add_edge("summarize_context_node", "conversation_node")
     # graph_builder.add_conditional_edges("connector_node", should_summarize_conversation)
     # graph_builder.add_edge("summarize_conversation_node", END)
-    graph_builder.add_edge("conversation_node", END)
     
     return graph_builder
 
--- philoagents-api/src/philoagents/application/conversation_service/workflow/cluedo_nodes.py
+++ philoagents-api/src/philoagents/application/conversation_service/workflow/cluedo_nodes.py
@@ -1,6 +1,8 @@
-import json
-from langchain_core.messages import RemoveMessage
+from typing import Literal
+from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, SystemMessage
 from langchain_core.runnables import RunnableConfig
+
+from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode
 
 from philoagents.application.conversation_service.workflow.cluedo_chains import (
@@ -39,9 +41,20 @@ async def crime_case_node(state: SuspectState, config: RunnableConfig):
     state["crime_scene"] = crime_case.crime_scene
     state["victim"] = crime_case.victim
     state["culprit"] = crime_case.culprit
-    return crime_case
+    state["messages"] = add_messages(
+        left=state["messages"],
+        right=[
+            SystemMessage(content=f"The crime case is: {crime_case.case_description}"),
+            SystemMessage(content=f"Summary of the case: {crime_case.summary_case}"),
+            SystemMessage(content=f"Weapon used: {crime_case.weapon}"),
+            SystemMessage(content=f"Location of the crime: {crime_case.location}"),
+            SystemMessage(content=f"Crime scene description: {crime_case.crime_scene}"),
+            SystemMessage(content=f"Victim: {crime_case.victim}"),
+            SystemMessage(content=f"Culprit: {crime_case.culprit}"),
+        ])
+    return state
 
-async def select_suspect_node(state: SuspectState):
+def select_suspect_node(state: SuspectState):
     """
     Select a suspect from the available suspects and set the state with the suspect's context, name, perspective, and style.
     This function lists all available suspects, prompts the user to select one, and updates the state accordingly.
@@ -56,7 +69,7 @@ async def select_suspect_node(state: SuspectState):
     suspect_list = "\n".join(
         f"- {suspect.id}: {suspect.name}" for suspect in suspects
     )
-    print (f"Available suspects:\n{suspect_list}")
+    print(f"Please, choose the suspect. Available suspects:\n{suspect_list}")
     # Ask the user to select a suspect
     selected_suspect_id = input("Please enter the ID of the suspect you want to talk to: ")
     if selected_suspect_id not in [suspect.id for suspect in suspects]:
@@ -68,7 +81,67 @@ async def select_suspect_node(state: SuspectState):
     state["suspect_name"] = suspect.name
     state["suspect_perspective"] = suspect.perspective
     state["suspect_style"] = suspect.style
+    state["messages"] = add_messages(
+        left=state["messages"],
+        right=[AIMessage(content=f"The player has selected a {suspect.name} suspect. Remember acting like {suspect.name}!")]
+    )
     return state
+
+def get_question_node(state: SuspectState):
+    """
+    Get the question from the user to ask the suspect.
+    This function prompts the user to enter a question they want to ask the suspect.
+    Args:
+        state (SuspectState): The current state of the conversation.
+    Returns:
+        Literal["get_question_node", "get_decision_continue_conversation_node"]:
+        The next node to execute, which is either to get a question or to decide whether to continue the conversation.
+    Raises:
+        ValueError: If the user input is empty or invalid.
+    """
+    question = input(f"Please, enter your question for {state['suspect_name']}: ")
+    state["messages"] = add_messages(
+        left=state["messages"],
+        right=[HumanMessage(content=f"Detective: {question}.")]
+    )
+    return state
+
+def get_decision_continue_conversation_node(state: SuspectState) -> Literal["get_decision_select_or_exit_node",
+                                                                            "get_decision_continue_conversation_node"]:
+    """
+    Get the decision from the user to continue the conversation or not.
+    This function prompts the user to decide whether they want to continue the conversation with the suspect.
+    Args:
+        state (SuspectState): The current state of the conversation.
+    Returns:
+        SuspectState: The updated state with the user's decision.
+    """
+    decision = input(f"Do you want to continue the conversation with the suspect {state['suspect_name']}? (yes/no): ").strip().lower()
+    if not decision or decision == 'yes':
+        return "get_question_node"
+    if decision not in ["yes", "no"]:
+        raise ValueError("Invalid decision. Please enter 'yes' or 'no'.")
+    if decision == "no":
+        return "get_decision_select_or_exit_node"
+    
+def get_decision_select_or_exit_node(state: SuspectState) -> Literal["select_suspect_node", "END"]:
+    """
+    Get the decision from the user to select another suspect or exit the conversation.
+    This function prompts the user to decide whether they want to select another suspect or exit the conversation.
+    Args:
+        state (SuspectState): The current state of the conversation.
+    Returns:
+        Literal["select_suspect_node", "END"]:
+        The next node to execute, which is either to select another suspect or to end the conversation.
+    Raises:
+        ValueError: If the user input is invalid.
+    """
+    decision = input("Do you want to select another suspect or exit? (select/exit): ").strip().lower()
+    if decision == "select":
+        return "select_suspect_node"
+    if decision == "exit":
+        return "END"
+    raise ValueError("Invalid decision. Please enter 'select' or 'exit'.")
 
 async def conversation_node(state: SuspectState, config: RunnableConfig):
     summary = state.get("summary", "")
@@ -86,8 +159,12 @@ async def conversation_node(state: SuspectState, config: RunnableConfig):
         },
         config,
     )
-    
-    return {"messages": response}
+    print(f"{state['suspect_name']}: {response.content}")
+    state["messages"] = add_messages(
+        left=state["messages"],
+        right=[AIMessage(content=response.content)]
+    )
+    return state
 
 
 # async def summarize_conversation_node(state: SuspectState):
